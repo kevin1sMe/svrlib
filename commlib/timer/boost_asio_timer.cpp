1246b8a95bb7982f07b6e70add8d70cb917c9139
kevinlin <linjiang1205@qq.com>
2015-02-28 15:50:56 +0800

    为什么使用cmake的 find_library()一直找到的是*.dylib而不是*.a呢？

---
+++ commlib/timer/boost_asio_timer.cpp
@@ -0,0 +1,55 @@
+//来自boost asio的示例程序
+#include <iostream>  
+#include <boost/asio.hpp>  
+#include <boost/date_time/posix_time/posix_time.hpp>  
+#include <boost/bind.hpp>  
+
+using namespace std;
+
+  
+void print1(const boost::system::error_code& /*e*/,  
+        boost::asio::deadline_timer* t, int* count)  
+{  
+    if (*count < 5)  
+    {  
+        cout << *count << "\n" << endl;  
+        ++(*count);  
+  
+        t->expires_at(t->expires_at() + boost::posix_time::seconds(1));  
+  
+        t->async_wait(boost::bind(print1, boost::asio::placeholders::error, t, count));  
+    }  
+  
+}  
+  
+void tuttimer3()  
+{  
+    boost::asio::io_service io;  
+    int count = 0;  
+    boost::asio::deadline_timer t(io, boost::posix_time::seconds(1));  
+  
+    t.async_wait(boost::bind(print1, boost::asio::placeholders::error, &t, &count));  
+  
+    io.run();  
+  
+    cout << "Final count is " << count << "\n" << endl;  
+}  
+  
+int main() {  
+    tuttimer3();  
+    return 0;  
+}  
+/* 
+运行结果： 
+0 
+ 
+1 
+ 
+2 
+ 
+3 
+ 
+4 
+ 
+Final count is 5 
+*/
